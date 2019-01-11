f53699e6fb9562744d3ef64fd665e870f38b7383
legacy-collaborator-7770 <johnvasiliadis@me.com>
2019-01-11 19:09:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+
+  array = []
+
+  for (i=0; i<=musicians.length; i++){
+    sentence = musicians[i] + " plays " + instruments[i]
+    array = [...array, sentence]
+  }
+    
+}
