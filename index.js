4cd7af35421c43df26effef139c862c5be4987ba
legacy-collaborator-7770 <johnvasiliadis@me.com>
2019-01-11 19:20:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function theBeatlesPlay(musicians, instruments) {
 
   let array = []
 
-  for (let i=0; i<=musicians.length; i++){
+  for (let i=0; i<musicians.length; i++){
     let sentence = musicians[i] + " plays " + instruments[i]
     array = [...array, sentence]
   }
