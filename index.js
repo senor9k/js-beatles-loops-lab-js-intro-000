6987a0920d07ff3066a20cb362c5fe09aa83c541
legacy-collaborator-7770 <johnvasiliadis@me.com>
2019-01-11 19:30:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+
+  let array = []
+
+  for (let i = 0; i < musicians.length; i++){
+    let sentence = musicians[i] + " plays " + instruments[i]
+    array = [...array, sentence]
+  }
+  return array
+}
+
+
+function johnLennonFacts(facts){
+  let array = []
+  let i = 0
+  while (i < facts.length){
+    let sentence = facts[i] + "!!!"
+    array = [...array, sentence]
+    i++
+  }
+  return array
+}
+
+function iLoveTheBeatles(number){
+  let array = []
+  do {
+    array = [...array, "I love the Beatles!"]
+    number++
+  } while (number < 15)
+    return array
+}
